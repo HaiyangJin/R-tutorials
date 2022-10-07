39a6d254beea55aefc822d3da2967ad6626d78bb
Haiyang <hjin317@aucklanduni.ac.nz>
2022-10-07 11:23:25 +0400

    sort archive

--- My_subdir/README.md
+++
@@ -1,2 +0,0 @@
-# My_R_Scripts
-R scripts used for my data analysis and plots.
--- My_subdir/function/ezStatsPlus.R
+++
@@ -1,9 +0,0 @@
-# Create a function called ezStatsPlus to calculate the SE, CI, hi(gh), lo(w).
-ezStatsPlus <- function(df){
-  df %>%
-    mutate(SE = SD / sqrt(N)) %>%  # add standard error
-    mutate(hi = Mean + SE) %>% #
-    mutate(lo = Mean - SE) %>%
-    mutate(CI = SE * qt(0.975,N)) %>%  # calculation 1 
-    mutate(ezCI = FLSD / sqrt(2))   # calculation 2
-}
